ece7e0e19e59c3977d0ea4b2c3946d7ac81d0bd9
nyradhr <alessio.piccione@hotmail.it>
2025-01-27 23:38:08 +0100

    extrapolated function to copy files from src to dst in its own module

--- src/main.py
+++ src/main.py
@@ -1,25 +1,4 @@
-import shutil
-import os
-
-def copy_src_to_dst(src, dst):
-    if not os.path.exists(src) or not os.path.exists(dst):
-        return
-    shutil.rmtree(dst)
-    os.mkdir(dst)
-    copy_files_rec(src, dst)
-
-def copy_files_rec(src, dst):
-    content = os.listdir(src)
-    if len(content) == 0:
-        return
-    for elem in content:
-        to_copy = os.path.join(src, elem)
-        if os.path.isfile(to_copy):
-            shutil.copy(to_copy, dst)
-            print(to_copy)
-        else:
-            os.mkdir(os.path.join(dst, elem))
-            copy_files_rec(to_copy, os.path.join(dst, elem))
+from copy_src_to_dst import copy_src_to_dst
 
 def main():
     copy_src_to_dst("./static", "./public")
